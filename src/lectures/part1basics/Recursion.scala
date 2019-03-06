983aaeaa1263f0c9949caca2579d46da37d483e5
Hawk87 <tidus.rm@libero.it>
2019-03-06 23:37:46 +0100

    Recursion

---
+++ src/lectures/part1basics/Recursion.scala
@@ -0,0 +1,52 @@
+package lectures.part1basics
+
+import scala.annotation.tailrec
+
+object Recursion extends App {
+  def factorial(n: Int): Int =
+    if (n <= 1) 1
+    else {
+      println("Computing factorial of " + n + " - I first need factorial of " + (n - 1))
+      val result = n * factorial(n - 1)
+      println("Computed factorial of " + n)
+
+      result
+    }
+
+  println(factorial(10))
+  //  println(factorial(5000))
+
+  def anotherFactorial(n: Int): BigInt = {
+    @tailrec
+    def factHelper(x: Int, accumulator: BigInt): BigInt =
+      if (x <= 1) accumulator
+      else factHelper(x - 1, x * accumulator) // TAIL RECURSION = use recursive call as the LAST expression
+
+    factHelper(n, 1)
+  }
+
+  /*
+    anotherFactorial(10) = factHelper(10, 1)
+    = factHelper(9, 10 * 1)
+    = factHelper(8, 9 * 10 * 1)
+    = factHelper(8, 9 * 10 * 1)
+    = ...
+    = factHelper(2, 3 * 4 * ... * 10 * 1)
+    = factHelper(1, 1 * 2 * 3 * 4 * ... * 10)
+    = 1 * 2 * 3 * 4 * ... * 10
+  */
+
+  println(anotherFactorial(5000))
+
+  // WHEN YOU NEED LOOPS, USE _TAIL_ RECURSION.
+
+  // ANY FUNCTION can be turned in a TAIL RECURSIVE function.
+
+  /*
+    1. Concatenate a string n times
+    2. isPrime function tail recursive
+    3. Fibonacci function tail recursive
+
+    HINT: use as many accumulators as you have recursive calls.
+  */
+}
