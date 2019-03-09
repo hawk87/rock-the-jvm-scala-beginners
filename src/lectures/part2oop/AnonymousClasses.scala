de068946547263f5f96dc7f9d475396631ae5a83
Hawk87 <tidus.rm@libero.it>
2019-03-09 23:08:50 +0100

    Implemented map, flatMap and filter

--- src/lectures/part2oop/AnonymousClasses.scala
+++ src/lectures/part2oop/AnonymousClasses.scala
@@ -30,4 +30,17 @@ object AnonymousClasses extends App {
     def sayHi: Unit = println(s"Hi, my name is $name, how can I help?")
   }
 
+  /*
+    1. Generic trait MyPredicate[-T] with a little method test(T) => Boolean
+    2. Generic trait MyTransformer[-A, B] with a method transform(A) => B
+    3. MyList:
+      - map(transformer) => MyList
+      - filter(predicate) => MyList
+      - flatMap(transformer from A to MyList[B]) => MyList[B]
+
+      [1,2,3].map(n * 2) = [2,4,6]
+      [1,2,3,4].filter(n % 2) = [2,4]
+      [1,2,3].flatMap(n => [n,n+1]) = [1,2,2,3,3,4]
+   */
+
 }
