a64d4136528e8afc52a4f64549376dca76c9ee61
Hawk87 <tidus.rm@libero.it>
2019-03-10 15:03:08 +0100

    Packaging and imports

---
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -0,0 +1,37 @@
+package lectures.part2oop
+
+import java.sql.{Date => SqlDate}
+import java.util.Date
+
+import playground.{PrinceCharming, Cinderella => Princess}
+
+object PackagingAndImports extends App {
+
+  // package members are accessible by their simple name
+  val writer = new Writer("Daniel", "RockTheJVM", 2018)
+
+  // import the package
+  val princess = new Princess // playground.Cinderella = fully qualified name
+
+  // packages are in hierarchy
+  // matching folder structure.
+
+  // package object
+  sayHello
+  println(SPEED_OF_LIGHT)
+
+  // imports
+  val prince = new PrinceCharming
+
+  // 1. use FQ names
+  val date = new Date
+  val sqlDate1 = new java.sql.Date(2018, 5, 4)
+
+  // 2. use aliasing
+  val sqlDate2 = new SqlDate(2018, 5, 4)
+
+  // default imports
+  // java.lang - String, Object, Exception
+  // scala - Int, Nothing, Function
+  // scala.Predef - println, ???
+}
---
+++ src/lectures/part2oop/package.scala
@@ -0,0 +1,6 @@
+package lectures
+
+package object part2oop {
+  val SPEED_OF_LIGHT = 299792458
+  def sayHello: Unit = println("Hello, Scala")
+}
