eec6425b47ce277908b2f4709c7b00d61dceff1b
Hawk87 <tidus.rm@libero.it>
2019-03-21 12:56:58 +0100

    Cleanup

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -12,21 +12,12 @@ object HOFsCurries extends App {
   // nTimes(f, 3, x) = f(f(f(x))) = nTimes(f, 2, f(x))
   // nTimes(f, n, x) = f(f(...(x))) = nTimes(f, n-1, f(x))
 
-  val plusOne = (x: Int) => x + 1
-  val plus10 = nTimesBetter(plusOne, 10)
-  println(nTimes(plusOne, 10, 1))
-  // curried functions
-  val superAdder: Int => (Int => Int) = (x: Int) => ((y: Int) => x + y)
-  val add3 = superAdder(3) // y => 3 + y
-  println(plus10(1))
-  val standardFormat: Double => String = curriedFormatter("%4.2f")
-  val preciseFormat: Double => String = curriedFormatter("%10.8f")
-  println(add3(10))
-  println(superAdder(3)(10))
-
   def nTimes(f: Int => Int, n: Int, x: Int): Int =
     if (n <= 0) x
     else nTimes(f, n - 1, f(x))
+
+  val plusOne = (x: Int) => x + 1
+  println(nTimes(plusOne, 10, 1))
 
   // ntb(f,n) = x => f(f(f...(x)))
   // increment10 = ntb(plusOne, 10) = x => plusOne(plusOne(...(x)))
@@ -35,8 +26,20 @@ object HOFsCurries extends App {
     if (n <= 0) (x: Int) => x
     else (x: Int) => nTimesBetter(f, n - 1)(f(x))
 
+  val plus10 = nTimesBetter(plusOne, 10)
+  println(plus10(1))
+
+  // curried functions
+  val superAdder: Int => (Int => Int) = (x: Int) => ((y: Int) => x + y)
+  val add3 = superAdder(3) // y => 3 + y
+  println(add3(10))
+  println(superAdder(3)(10))
+
   // functions with multiple parameter lists
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  val standardFormat: Double => String = curriedFormatter("%4.2f")
+  val preciseFormat: Double => String = curriedFormatter("%10.8f")
 
   println(standardFormat(Math.PI))
   println(preciseFormat(Math.PI))
